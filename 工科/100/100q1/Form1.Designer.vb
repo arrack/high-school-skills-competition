65eaa88a0666fded523771584c1c88fdae453175
Arrack <zeng.mz+k12ea@gmail.com>
2019-07-28 00:43:08 +0800

    add 100q1

---
+++ 工科/100/100q1/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtN = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtC = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.Location = New System.Drawing.Point(1, 2)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(665, 512)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 545)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "N="
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(249, 540)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Draw"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtN
+        '
+        Me.txtN.Location = New System.Drawing.Point(73, 541)
+        Me.txtN.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtN.Name = "txtN"
+        Me.txtN.Size = New System.Drawing.Size(132, 25)
+        Me.txtN.TabIndex = 3
+        Me.txtN.Text = "3"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 595)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Color = "
+        '
+        'txtC
+        '
+        Me.txtC.Location = New System.Drawing.Point(73, 591)
+        Me.txtC.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtC.Name = "txtC"
+        Me.txtC.Size = New System.Drawing.Size(132, 25)
+        Me.txtC.TabIndex = 3
+        Me.txtC.Text = "B"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 300
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(427, 535)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(124, 52)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "練習1 直線"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(674, 2)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(273, 514)
+        Me.ListBox1.TabIndex = 8
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(559, 535)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(124, 52)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "練習2 三角形"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(691, 535)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(124, 52)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "練習3 四邊"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(823, 535)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(124, 52)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "練習4 圓"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(427, 612)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(124, 52)
+        Me.Button6.TabIndex = 12
+        Me.Button6.Text = "練習5 派"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(962, 691)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtC)
+        Me.Controls.Add(Me.txtN)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtN As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtC As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+
+End Class
---
+++ 工科/100/100q1/Form1.vb
@@ -0,0 +1,140 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        Dim n As Integer = 3
+        Dim c As String = txtC.Text
+        Dim pen As New Pen(Color.Black)
+        Dim p As Point = New Point(200, 200)
+
+        g.Clear(Color.White)
+        n = Val(txtN.Text)
+
+        Select Case c
+            Case "R"
+                pen.Color = Color.Red
+            Case "G"
+                pen.Color = Color.Green
+            Case "L"
+                pen.Color = Color.Blue
+        End Select
+
+        ListBox1.Items.Clear()
+
+        For i = 0 To n - 1
+            ListBox1.Items.Add(getNewPoint(p, 360 / n * i, 200))
+            'g.DrawLine(pen, getNewPoint(p, 360 / n * i - 90, 200), getNewPoint(p, 360 / n * (i + 1) - 90, 200))
+
+            For j = i + 1 To n - 1
+                g.DrawLine(pen, _
+                    getNewPoint(p, 360 / n * i, 200), _
+                    getNewPoint(p, 360 / n * j, 200))
+                Thread.Sleep(100)
+            Next
+        Next
+        'g.DrawEllipse(pen, p.X - 200, p.Y - 200, 400, 400)
+    End Sub
+
+    Private Function getNewPoint(ByVal point As Point, ByVal angle As Integer, ByVal length As Integer)
+        '弧度 = 角度 * (Math.PI / 180)
+
+        point.X = point.X + Math.Cos(angle * Math.PI / 180) * length
+        point.Y = point.Y - Math.Sin(angle * Math.PI / 180) * length
+
+        Return point
+    End Function
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        '建立 Graphics
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        '建立筆
+        Dim pen As New Pen(Color.Black)
+        '建立點
+        Dim p1 As New Point(100, 100)
+        Dim p2 As New Point(200, 200)
+        '畫線
+        g.DrawLine(pen, p1, p2)
+
+        ' 計算後
+        Dim p3 As Point = getNewPoint(p1, 45, 100)
+        g.DrawLine(pen, p1, p3)
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        Dim pen As New Pen(Color.Black)
+        Dim pen2 As New Pen(Color.Red)
+
+        Dim p As New Point(100, 100)
+        Dim p1 As Point = getNewPoint(p, 0, 100)
+        Dim p2 As Point = getNewPoint(p, 120, 100)
+        Dim p3 As Point = getNewPoint(p, 240, 100)
+
+
+        g.DrawLine(pen, p1, p2)
+        Thread.Sleep(100)
+        g.DrawLine(pen, p2, p3)
+        Thread.Sleep(100)
+        g.DrawLine(pen, p3, p1)
+        Thread.Sleep(100)
+
+        g.DrawLine(pen2, p, p1)
+        g.DrawLine(pen2, p, p2)
+        g.DrawLine(pen2, p, p3)
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        Dim pen As New Pen(Color.Black)
+
+        Dim p1 As New Point(200, 200)
+        Dim p2 As Point = getNewPoint(p1, 0, 100)
+        Dim p3 As Point = getNewPoint(p2, 90, 100)
+        Dim p4 As Point = getNewPoint(p3, 180, 100)
+
+        g.DrawLine(pen, p1, p2)
+        g.DrawLine(pen, p2, p3)
+        g.DrawLine(pen, p3, p4)
+        g.DrawLine(pen, p4, p1)
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        Dim Pen As New Pen(Color.Blue)
+
+        'x y width height
+        g.DrawEllipse(Pen, 100, 100, 300, 300)
+        'g.DrawRectangle(Pen, 100, 100, 300, 300)
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        Dim Pen As New Pen(Color.Blue)
+        'DrawPie (Pen, x, y, 寬度, 高度, 起始角度, 角度變化)
+        g.DrawPie(Pen, 100, 100, 300, 300, 45, 90)
+    End Sub
+
+    '逐步寫
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        Dim pen As New Pen(Color.Black)
+        Dim p As Point = New Point(200, 200)
+
+        g.Clear(Color.White)
+
+        g.DrawLine(pen, p, getNewPoint(p, 90 - Now.Second * 6, 100))
+        g.DrawLine(pen, p, getNewPoint(p, 90 - Now.Hour * 30, 80))
+        g.DrawLine(pen, p, getNewPoint(p, 90 - Now.Minute * 6, 90))
+
+        g.DrawEllipse(pen, p.X - 100, p.Y - 100, 200, 200)
+
+        Dim font As New Font("Arial", 18, FontStyle.Bold)
+        For i = 1 To 12
+            Dim np As Point = getNewPoint(p, 90 - i * 30, 140)
+            g.DrawString(i, font, Brushes.Black, np.X - 10, np.Y - 10)
+        Next
+    End Sub
+End Class
